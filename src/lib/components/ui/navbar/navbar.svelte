cb28e200a14884e308920ec781fa8e565f764ac6
Alan Le <36429067+mikumino@users.noreply.github.com>
2024-03-04 15:58:53 -0800

    navbar style update

--- src/lib/components/ui/navbar/navbar.svelte
+++ src/lib/components/ui/navbar/navbar.svelte
@@ -2,9 +2,9 @@
     import { Button } from "$lib/components/ui/button";
 </script>
 <div class="m-4">
-    <div class="w-full bg-slate-100 rounded-lg drop-shadow-lg">
+    <div class="w-full bg-slate-100 rounded-lg outline-slate-300 outline-1 outline drop-shadow-lg">
         <div class="flex flex-row justify-between items-center p-4">
-            <h1 class="text-xl font-bold">OrgOrg</h1>
+            <a href="/" class="text-xl font-bold">OrgOrg</a>
             <div class="flex flex-row space-x-2">
                 <Button href="/organize">Organize</Button>
                 <Button href="/register" variant='outline'>Register</Button>
---
+++ src/routes/organize/+page.svelte
@@ -0,0 +1,5 @@
+<script>
+    import Navbar from "$lib/components/ui/navbar/navbar.svelte";
+</script>
+
+<Navbar />
