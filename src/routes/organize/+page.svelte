7688d126aa72587077260bdf4e8ec05e3280939c
Alan Le <36429067+mikumino@users.noreply.github.com>
2024-03-04 18:38:25 -0800

    organize page

--- src/routes/organize/+page.svelte
+++ src/routes/organize/+page.svelte
@@ -1,5 +1,28 @@
 <script>
-    import Navbar from "$lib/components/ui/navbar/navbar.svelte";
+    import Input from "$lib/components/ui/input/input.svelte";
+import Label from "$lib/components/ui/label/label.svelte";
+import Navbar from "$lib/components/ui/navbar/navbar.svelte";
 </script>
 
-<Navbar />
+<div class="h-screen">
+    <Navbar />
+    <div class="flex items-center ml-24 h-full">
+        <div class="flex flex-col space-y-6 w-1/2">
+            <h1 class="text-4xl font-bold">Organize meeting</h1>
+            <form>
+                <div class="flex flex-col space-y-4 mb-4">
+                    <Label for="meetingName">Meeting name</Label>
+                    <Input type="text" id="meetingName"/>
+                </div>
+                <div class="flex flex-col space-y-4">
+                    <Label for="timeRange">Time range</Label>
+                    <div class="flex flex-row space-x-4">
+                        <Input type="text" id="timeRange" placeholder="Start time"/>
+                        <p class="text-slate-500">to</p>
+                        <Input type="text" id="timeRange" placeholder="End time"/>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
