6d998165515037f60f7d96f931040713b3842b06
Alan Le <36429067+mikumino@users.noreply.github.com>
2024-03-19 14:34:36 -0700

    Make navbar not float

--- src/lib/components/ui/navbar/navbar.svelte
+++ src/lib/components/ui/navbar/navbar.svelte
@@ -1,15 +1,13 @@
 <script>
     import { Button } from "$lib/components/ui/button";
 </script>
-<div class="m-4">
-    <div class="w-full bg-slate-100 rounded-lg outline-slate-300 outline-1 outline">
-        <div class="flex flex-row justify-between items-center p-4">
-            <a href="/" class="text-xl font-bold">OrgOrg</a>
-            <div class="flex flex-row space-x-2">
-                <Button href="/organize">Organize</Button>
-                <Button href="/register" variant='outline'>Register</Button>
-                <Button href="/login" variant='outline'>Login</Button>
-            </div>
+<div class="w-full outline-slate-300 shadow-md outline-1 outline">
+    <div class="flex flex-row justify-between items-center mx-4 p-4">
+        <a href="/" class="text-xl font-bold">OrgOrg</a>
+        <div class="flex flex-row space-x-2">
+            <Button href="/organize">Organize</Button>
+            <Button href="/register" variant='outline'>Register</Button>
+            <Button href="/login" variant='outline'>Login</Button>
         </div>
     </div>
 </div>
