0f66f3a2f14a317c6c32106f9482e399d07ec70d
Alan Le <36429067+mikumino@users.noreply.github.com>
2024-03-19 10:43:26 -0700

    Time picker

--- src/routes/organize/+page.svelte
+++ src/routes/organize/+page.svelte
@@ -4,10 +4,14 @@
     import Navbar from "$lib/components/ui/navbar/navbar.svelte";
 </script>
 
+<style>
+    
+</style>
+
 <div class="h-screen">
     <Navbar />
-    <div class="flex items-center ml-24 h-full">
-        <div class="flex flex-col space-y-6 w-1/2">
+    <div class="flex mt-24 ml-24 h-full">
+        <div class="flex flex-col space-y-6 w-1/3">
             <h1 class="text-4xl font-bold">Organize meeting</h1>
             <form>
                 <div class="flex flex-col space-y-4 mb-4 w-96">
@@ -17,9 +21,9 @@
                 <div class="flex flex-col space-y-4">
                     <Label for="timeRange">Time range</Label>
                     <div class="flex flex-row space-x-4">
-                        <Input type="text" id="timeRange" placeholder="Start time"/>
+                        <Input id="startTime" class='w-fit' type="time" />
                         <p class="text-slate-500">to</p>
-                        <Input type="text" id="timeRange" placeholder="End time"/>
+                        <Input id="endTime" class='w-fit' type="time" />
                     </div>
                 </div>
             </form>
