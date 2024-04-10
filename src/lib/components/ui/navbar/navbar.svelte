22473d4717ca570bc8b5fd013d80b3ffb5033e2f
Alan Le <36429067+mikumino@users.noreply.github.com>
2024-04-10 16:48:52 -0700

    ui changes

--- src/lib/components/ui/navbar/navbar.svelte
+++ src/lib/components/ui/navbar/navbar.svelte
@@ -1,7 +1,7 @@
 <script>
     import { Button } from "$lib/components/ui/button";
 </script>
-<div class="w-full outline-slate-300 shadow-md outline-1 outline">
+<div class="fixed w-full outline-slate-300 shadow-md outline-1 outline">
     <div class="flex flex-row justify-between items-center mx-4 p-4">
         <a href="/" class="text-xl font-bold">OrgOrg</a>
         <div class="flex flex-row space-x-2">
--- src/routes/organize/+page.svelte
+++ src/routes/organize/+page.svelte
@@ -3,6 +3,7 @@
     import Label from "$lib/components/ui/label/label.svelte";
     import Navbar from "$lib/components/ui/navbar/navbar.svelte";
     import DatePicker from "$lib/components/ui/datepicker/datepicker.svelte";
+	import Button from "$lib/components/ui/button/button.svelte";
 </script>
 
 <style>
@@ -11,15 +12,15 @@
 
 <div class="h-screen">
     <Navbar />
-    <div class="flex flex-row mt-24 ml-24 h-full">
-        <div class="flex flex-col space-y-6 w-1/3">
+    <div class="flex flex-row items-center justify-center h-full">
+        <div class="flex flex-col space-y-6 mr-12">
             <h1 class="text-4xl font-bold">Organize meeting</h1>
-            <form>
+            <form class="w-fit">
                 <div class="flex flex-col space-y-4 mb-4 w-96">
                     <Label for="meetingName">Meeting name</Label>
                     <Input type="text" id="meetingName"/>
                 </div>
-                <div class="flex flex-col space-y-4">
+                <div class="flex flex-col space-y-4 mb-4">
                     <Label for="timeRange">Time range</Label>
                     <div class="flex flex-row space-x-4">
                         <Input id="startTime" class='w-fit' type="time" />
@@ -27,6 +28,7 @@
                         <Input id="endTime" class='w-fit' type="time" />
                     </div>
                 </div>
+                <Button>Create meeting</Button>
             </form>
         </div>
         <div>
