172023ad4c7a692e5cb95be0c6bbb0112ed4ce73
laurencon <101435564+laurencon@users.noreply.github.com>
2024-04-15 13:49:52 -0700

    Delete supabase.js

--- supabase.js
+++
@@ -1,6 +0,0 @@
-
-import { createClient } from '@supabase/supabase-js'
-const supabaseUrl = 'https://qqrbajmyoepghldvrlja.supabase.co'
-const supabaseKey = process.env.SUPABASE_KEY
-const supabase = createClient(supabaseUrl, supabaseKey)
-export default supabase;
