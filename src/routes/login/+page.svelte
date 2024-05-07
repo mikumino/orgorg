e0a14df0999022bdc96b6a4e62dea40d82f77518
Alan Le <36429067+mikumino@users.noreply.github.com>
2024-05-06 19:12:12 -0700

    log in

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,4 +1,5 @@
 <script>
+	import { goto } from "$app/navigation";
     import { Button } from "$lib/components/ui/button";
     import * as Card from "$lib/components/ui/card";
     import { Input } from "$lib/components/ui/input";
@@ -6,44 +7,47 @@
     import { supabase } from "../../supabaseClient";
 
     let loading = false;
-    let email = "";
+    let email, password;
 
     const handleLogin = async () => {
         console.log("handleLogin");
         try {
             loading = true;
-            const { error } = await supabase.auth.signInWithOtp({ email });
+            const { error } = await supabase.auth.signInWithPassword({ email: email, password: password });
             if (error) throw error;
-            alert("Check your email for the login link!");
         } catch (error) {
-            if (error instanceof Error) {
-                alert(error.message);
-            }
+            console.error("Error logging in:", error.message);
         } finally {
             loading = false;
+            goto("/");
         }
     }
 </script>
 
 <div class="flex h-screen items-center justify-center">
     <Card.Root class="w-[350px] drop-shadow-lg">
+        <form on:submit|preventDefault={handleLogin}>
         <Card.Header>
             <h1 class="text-2xl font-bold">Login</h1>
         </Card.Header>
         <Card.Content>
-            <form on:submit|preventDefault={handleLogin}>
                 <div class="flex flex-col space-y-4">
-                    <div class="flex flex-col space-y-2">
+                    <div class="flex flex-col space-y-4">
                         <Label for="email">Email</Label>
                         <Input type="email" id="email" bind:value={email} />
+                        <Label for="password">Password</Label>
+                        <Input type="password" id="password" bind:value={password} />
                     </div>
                     <div class="flex flex-row">
                     </div>
                 </div>
-                <Button type="submit">Login</Button>
-            </form>
-        </Card.Content>
-        <Card.Footer>
-        </Card.Footer>
+            </Card.Content>
+            <Card.Footer>
+                <div class="flex flex-col space-y-2">
+                    <Button type="submit">Login</Button>
+                    <p class="text-slate-500">Don't have an account? <a class="underline" href="/login">Sign up</a></p>
+                </div>
+            </Card.Footer>
+        </form>
     </Card.Root>
 </div>
