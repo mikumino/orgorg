037be5ed46599024d9312a8f71719e21d78a6e0c
Alan Le <36429067+mikumino@users.noreply.github.com>
2024-05-08 16:18:15 -0700

    use process env

--- src/supabaseServerClient.ts
+++ src/supabaseServerClient.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 import { env } from '$lib/env'
 
-const supabaseUrl = env.VITE_SUPABASE_URL
-const supabaseAnonKey = env.VITE_SUPABASE_ANON_KEY
+const supabaseUrl = process.env.VITE_SUPABASE_URL
+const supabaseAnonKey = process.env.VITE_SUPABASE_ANON_KEY
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
