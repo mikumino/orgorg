0ddd18b585d8729c448b3f21a745cc006a8e2e2e
Alan Le <36429067+mikumino@users.noreply.github.com>
2024-05-08 16:31:35 -0700

    Update supabaseServerClient.ts

--- src/supabaseServerClient.ts
+++ src/supabaseServerClient.ts
@@ -1,5 +1,4 @@
 import { createClient } from '@supabase/supabase-js'
-import { env } from '$lib/env'
 
 const supabaseUrl = process.env.VITE_SUPABASE_URL
 const supabaseAnonKey = process.env.VITE_SUPABASE_ANON_KEY
